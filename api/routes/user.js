8ce0f80a7bcca49fea89f67ab825c30d6abef6e9
Baljeet Singh <113023797+Baljeet975@users.noreply.github.com>
2023-02-01 18:54:00 +0530

    c

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,77 +6,58 @@ const jwt = require("jsonwebtoken");
 const nodemailer = require("nodemailer");
 const User = require("../model/user");
 
-router.post("/signup", (req, res, next) => {
-  const user = new User({
-    _id: new mongoose.Types.ObjectId(),
-    number: req.body.number,
-    // name: req.body.name,
-    // password: req.body.password,
-    // mobilenumber: req.body.mobilenumber,
-  });
+router.post("/login", (req, res, next) => {
+  User.find({ number: req.body.number })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        const user = new User({
+          _id: new mongoose.Types.ObjectId(),
+          number: req.body.number,
+        });
+        user.save().then((result) => {
+          console.log(result, "rrrrrrr");
+          res.status(200).json({
+            new_user: result,
+          });
+        });
+      }
+      console.log("going forward");
+      if (req.body.number !== user[0].number) {
+        console.log("req.body.number", req.body.number);
+        console.log("user[0].number", user[0].number);
 
-  user
-    .save()
-    .then((result) => {
-      console.log(result, "rrrrrrr");
-      res.status(200).json({
-        new_user: result,
-      });
+        return res.status(401).json({
+          msg: "user number matching fail",
+        });
+      } else {
+        const token = jwt.sign(
+          {
+            _id: user[0]._id,
+
+            number: user[0].number,
+          },
+          "this is dummy text", // SECRET KEY
+          {
+            expiresIn: "24h",
+          }
+        );
+        res.status(200).json({
+          _id: user[0]._id,
+
+          phone: user[0].phone,
+
+          token: token,
+        });
+        console.log("Success Number Matching");
+      }
     })
     .catch((err) => {
-      console.log(err, "eeeee");
-
       res.status(500).json({
-        error: err,
+        err: err,
       });
     });
 });
-
-// router.post("/login", (req, res, next) => {
-//   User.find({ number: req.body.number })
-//     .exec()
-//     .then((user) => {
-//       if (user.length < 1) {
-//         return res.status(401).json({
-//           msg: "user not exist",
-//         });
-//       }
-//       console.log("going forward");
-//       if (req.body.password !== user[0].password) {
-//         console.log("req.body.password", req.body.password);
-//         console.log("user[0].password", user[0].password);
-
-//         return res.status(401).json({
-//           msg: "user password matching fail",
-//         });
-//       } else {
-//         const token = jwt.sign(
-//           {
-//             _id: user[0]._id,
-
-//             number: user[0].number,
-//           },
-//           "this is dummy text", // SECRET KEY
-//           {
-//             expiresIn: "24h",
-//           }
-//         );
-//         res.status(200).json({
-//           _id: user[0]._id,
-
-//           phone: user[0].phone,
-
-//           token: token,
-//         });
-//         console.log("Success Password Matching");
-//       }
-//     })
-//     .catch((err) => {
-//       res.status(500).json({
-//         err: err,
-//       });
-//     });
-// });
 
 router.get("/", (req, res, next) => {
   console.log("fetching all data");
